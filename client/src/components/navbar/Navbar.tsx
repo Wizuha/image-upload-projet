92c5749e070f68fee6051a65f84b24818c7cd29b
Wizuha <williams.wandji@gmail.com>
2025-08-26 11:25:29 +0200

    first commit

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,17 +1,29 @@
 "use client";
+import Link from "next/link";
 import { ModeToggle } from "../ui/mode-toggle";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme } = useTheme();
   return (
     <nav className="min-w-screen shadow-sm p-4 px-5 border-b-1 dark:bg-[#121826]">
       <div className="flex items-center justify-between">
-        <div>
-          <img src="/logo.svg" alt="logo clair" className="block dark:hidden" />
-          <img
-            src="/logo-small.svg"
-            alt="logo sombre"
-            className="hidden dark:block"
-          />
+        <div className="flex items-center gap-2">
+          <Link href="/">
+            {theme === "dark" ? (
+              <img src="/logo-small.svg" alt="logo sombre" className="block" />
+            ) : (
+              <img src="/logo.svg" alt="logo" className="block" />
+            )}
+            <img
+              src="/logo-small.svg"
+              alt="logo sombre"
+              className="hidden dark:block"
+            />
+          </Link>
+          <Link href="/">
+            <a className="text-lg font-bold">Uploadcare</a>
+          </Link>
         </div>
         <ModeToggle />
       </div>
